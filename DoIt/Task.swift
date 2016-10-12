fd12b3919df52f5a3edc4291cc7bf45bcd559c3e
Danmister <dcorrea012@gmail.com>
2016-10-11 22:12:22 -0400

    Added ! and added future ability to add taks

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Daniel Correa on 10/11/16.
+//  Copyright © 2016 Repel. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
