4b83543adacfa56bdfee9b77a0159b22414bfe9f
Danmister <dcorrea012@gmail.com>
2016-11-20 19:40:00 -0500

    ayyy

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Daniel Correa on 10/12/16.
+//  Copyright © 2016 Repel. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addedTapped(_ sender: AnyObject) {
+        //create a task from the outlet infomation
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        //add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+        
+    }
+}
